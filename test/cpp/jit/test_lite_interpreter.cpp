d0d729a2f113d75dbdf76f6def8d89fa6c84bf4f
chenlai <chenlai@devvm7453.prn0.facebook.com>
2022-03-31 17:14:35 -0700

    Remove internal logic to handle bytecode version 3
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/57775
    
    
    The minimum supported bytecode version is updated from 3 to 4. We no longer support version 3 bytecode models.
    
    Why?
    * There are hacky codes in operator loading, that performs differently on one operator on the global bytecode version 3. Instead operator related metadata should be passed (for example, in #56845). To allow future development, we remove the hacky way first.
    * The bytecode version was bumped from 3 to 4 more than half a year ago. Since all the production models are all bumped to version 4, it's not practical to keep and maintain version 3. The risk to deprecate version 3 is low.
    
    Differential Revision: [D28270791](https://our.internmc.facebook.com/intern/diff/D28270791/)
    ghstack-source-id: 152759282

--- test/cpp/jit/test_lite_interpreter.cpp
+++ test/cpp/jit/test_lite_interpreter.cpp
@@ -1185,7 +1185,6 @@ TEST(RunTimeTest, ParseOperator) {
       function.get());
   parseOperators(
       std::move(*c10::ivalue::Tuple::create(operators)).elements(),
-      model_version,
       1,
       function.get());
   const size_t rsize = 5;
@@ -1568,7 +1567,6 @@ TEST(RunTimeTest, RuntimeCall) {
       foo.get());
   parseOperators(
       std::move(*c10::ivalue::Tuple::create(operatorsFoo)).elements(),
-      model_version,
       1,
       foo.get());
   parseConstants(
@@ -1585,7 +1583,6 @@ TEST(RunTimeTest, RuntimeCall) {
       call.get());
   parseOperators(
       std::move(*c10::ivalue::Tuple::create(operatorsCall)).elements(),
-      model_version,
       1,
       call.get());
   parseConstants(
@@ -2089,10 +2086,7 @@ TEST(LiteInterpreterUpgraderTest, Upgrader) {
     if (byteCodeFunctionWithOperator.function.get_code().operators_.empty()) {
       for (const auto& op : byteCodeFunctionWithOperator.operators) {
         byteCodeFunctionWithOperator.function.append_operator(
-            op.name,
-            op.overload_name,
-            op.num_specified_args,
-            caffe2::serialize::kMaxSupportedFileFormatVersion);
+            op.name, op.overload_name, op.num_specified_args);
       }
     }
     upgrader_functions.push_back(byteCodeFunctionWithOperator.function);
